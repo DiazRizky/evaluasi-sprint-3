5917b003aa5a96358490891962c8842b14afbe34
DiazRizky <diazrizkydiazrizky@gmail.com>
2021-08-07 10:33:34 +0700

    first commit

---
+++ soal1/soal1.js
@@ -0,0 +1,4 @@
+let santriIkhwan = ['Syofyan', 'Aldi', 'Kardi'];
+for(let i = 1; i < santriIkhwan.length; i++) {
+    console.log(i + '.' + santriIkhwan[i]);
+}
---
+++ soal2/soal2.js
@@ -0,0 +1,4 @@
+const asalSantri =  { "Sidik" : "Lampung", "Wandi" : "Prabumulih", "Rahmad" : "Medan", "Usamah" : "Ceribon", "Syofyan" : "Purwokerto", "Aldi" : "Kendari", "Kardi" : "Lombok" }
+for(let i = 1; i < asalSantri.length; i++) {
+    console.log(i + '.' + asalSantri[i]);
+}
